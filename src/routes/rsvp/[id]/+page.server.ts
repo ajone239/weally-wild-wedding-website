e7a0836105272d31e203876d47731c8b061d00f9
Austin Jones <ajones53.aj@gmail.com>
2026-07-14 08:58:54 -0400

    wip: blah blah blah

--- src/routes/rsvp/[id]/+page.server.ts
+++ src/routes/rsvp/[id]/+page.server.ts
@@ -2,6 +2,7 @@ import { db } from '$lib/server/db';
 import { guest, party, type Guest } from '$lib/server/db/schema';
 import { eq } from 'drizzle-orm';
 import type { PageServerLoad } from '../$types';
+import { error, type Actions } from '@sveltejs/kit';
 
 export const load: PageServerLoad = async ({ params }) => {
     const party_id = params.id;
@@ -9,6 +10,10 @@ export const load: PageServerLoad = async ({ params }) => {
     const the_party = await db.query.party.findFirst({
         where: eq(party.id, party_id)
     });
+
+    if (!the_party) {
+        error(404, "party not found");
+    }
 
     const guests: Guest[] = await db.query.guest.findMany({
         where: eq(guest.party_id, party_id)
@@ -19,3 +24,10 @@ export const load: PageServerLoad = async ({ params }) => {
         guests: guests
     };
 };
+
+export const actions: Actions = {
+    default: async ({ request }) => {
+        const data = await request.formData();
+        console.log(data);
+    }
+}
--- src/routes/rsvp/[id]/+page.svelte
+++ src/routes/rsvp/[id]/+page.svelte
@@ -1,11 +1,67 @@
 <script lang="ts">
-	let { data } = $props();
+	import { enhance } from '$app/forms';
+	import { invalidateAll } from '$app/navigation';
+
+	let { data, form } = $props();
+
+	let party = $state(structuredClone(data.party));
+	let guests = $state(structuredClone(data.guests));
+	let notes = $state('');
+
+	let serdes = $derived(JSON.stringify({ p: party, g: guests, n: notes }));
+
+	$effect(() => {
+		party = structuredClone(data.party);
+		guests = structuredClone(data.guests);
+	});
+
+	$inspect(party);
+	$inspect(guests);
 </script>
 
-<h2>Party: {data.party.name} ({data.party.id})</h2>
+<p>{form}</p>
 
-{#each data.guests as guest}
-	<p>{guest.name} -- {guest.is_rsvp}</p>
-{:else}
-	<p>No guests :(</p>
-{/each}
+<h2>Party: {party.name} ({party.id})</h2>
+
+<form method="POST" use:enhance>
+	<div>
+		<label>
+			Finalize:
+			<input type="checkbox" value={party.is_rsvp != 0} onchange={() => (party.is_rsvp ^= 1)} />
+		</label>
+	</div>
+
+	<div>
+		<h3>Guests</h3>
+		{#each guests as guest}
+			<ul>
+				<li>
+					<label>
+						{guest.name}:
+						<input
+							type="checkbox"
+							value={guest.is_rsvp != 0}
+							onchange={() => (guest.is_rsvp ^= 1)}
+						/>
+					</label>
+				</li>
+			</ul>
+		{:else}
+			<p>No guests :(</p>
+		{/each}
+	</div>
+
+	<div>
+		<h3>Notes:</h3>
+
+		<!-- TODO(ajone239): hook up -->
+		<textarea bind:value={notes}></textarea>
+	</div>
+
+	<input type="hidden" value={serdes} name="data" />
+
+	<div>
+		<button type="submit"> Submit </button>
+		<button onclick={() => invalidateAll()}> Clear </button>
+	</div>
+</form>
