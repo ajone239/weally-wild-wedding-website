8f91e8f858ae2ea5f787f60c4d72711b77f11d15
Austin Jones <ajones53.aj@gmail.com>
2026-07-15 09:04:53 -0400

    switch it

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,24 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.TURSO_DB_URL) throw new Error('DATABASE_URL is not set');
-
-export default defineConfig({
-    schema: './src/lib/server/db/schema.ts',
-    out: './migrations',
-    dialect: 'turso',
-    dbCredentials: {
-        url: process.env.TURSO_DB_URL,
-        authToken: process.env.TURSO_DB_AUTH_TOKEN
-    },
-    verbose: true,
-    strict: true
-});
+export default process.env.DB_MODE == 'local' ?
+    defineConfig({
+        schema: './src/lib/server/db/schema.ts',
+        out: './migrations',
+        dialect: 'sqlite',
+        dbCredentials: {
+            url: process.env.DATABASE_URL!,
+        },
+        verbose: true,
+        strict: true
+    }) :
+    defineConfig({
+        schema: './src/lib/server/db/schema.ts',
+        out: './migrations',
+        dialect: 'turso',
+        dbCredentials: {
+            url: process.env.TURSO_DB_URL!,
+            authToken: process.env.TURSO_DB_AUTH_TOKEN!
+        },
+        verbose: true,
+        strict: true
+    });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,11 +3,14 @@ import { createClient } from '@libsql/client';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.TURSO_DB_URL) throw new Error('DATABASE_URL is not set');
+const config = env.DB_MODE == 'local' ?
+    {
+        url: env.DATABASE_URL!,
+    } : {
+        url: env.TURSO_DB_URL!,
+        authToken: env.TURSO_DB_AUTH_TOKEN
+    };
 
-const client = createClient({
-    url: env.TURSO_DB_URL,
-    authToken: env.TURSO_DB_AUTH_TOKEN
-});
+const client = createClient(config);
 
 export const db = drizzle(client, { schema });
