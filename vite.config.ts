be03dfc2721692178cea0eba8dc3e20c451bc25e
Austin Jones <ajones53.aj@gmail.com>
2026-07-16 21:29:16 -0400

    format

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
                     sizes: [640, 828, 1200, 1920, 3840],
                     formats: ['image/avif', 'image/webp'],
                     minimumCacheTTL: 300,
-                    domains: ['example-app.vercel.app'],
+                    domains: ['example-app.vercel.app']
                 }
             }),
             preprocess: [mdsvex({ extensions: ['.svx', '.md'] })],
