58ffd50ac4b757d13e79f473246280111b318dc4
Austin Jones <ajones53.aj@gmail.com>
2026-07-25 17:41:10 -0400

    update faq

--- src/routes/demo/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import Seo from '$lib/components/Seo.svelte';
-</script>
-
-<Seo title="Demo" noindex />
-
-<a href="/demo/controls">controls</a>
--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import Seo from '$lib/components/Seo.svelte';
+    import guy from '$lib/assets/male_inspo.png';
+    import lady from '$lib/assets/female_inspo.png';
 </script>
 
 <Seo title="FAQ" description="Answers to common questions about RSVPing, attire, and plus ones." />
 
 <h1>Frequently Asked Questions:</h1>
 
-<h2>How to RSVP</h2>
+<h2>How do I RSVP?</h2>
 
 <p>
     Please go to the
@@ -17,7 +19,7 @@
 <h2>Plus Ones</h2>
 
 <p>
-    When RSVPing, you will see the people that are associated with your party. It is our hope to
+    When RSVP-ing, you will see the people that are associated with your party. It is our hope to
     keep the wedding intimate. So, we ask that you only plan to bring those listed in your party.
 </p>
 
@@ -25,9 +27,23 @@
 
 <p>
     Our ceremony will start at 3:00 p.m. EST on Nov. 14, 2027. We ask that guests get to the venue
-    30 minutes early to ease of seating.
+    15 minutes early to ease of seating.
 </p>
 
-<h2>Attire</h2>
+<h2>Of Booze and Babies</h2>
 
-<p>🚧 Under construction: Come back later for more info. 🚧</p>
+<p>
+    This event is kid friendly. Our plan is to keep the wedding semi-dry. There will be moderate
+    amounts of wine and champagne provided at the reception.
+</p>
+
+<h2>Dress Code: Garden Casual</h2>
+
+<p>
+    The inpiration below should serve as direction and is not mandatory. However, please do refrain
+    from wearing white :).
+</p>
+
+<img style="margin-bottom: 1rem;" src={lady} alt="Feminine inspo for wedding attire." />
+
+<img src={guy} alt="Masculine inspo for wedding attire." />
